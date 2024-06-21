49ee7afc10b08aa20b19ba005ee36aab156a21d2
Arseniy Graur <me@argraur.dev>
2024-06-21 22:17:27 +0300

    Show deadlines and deadline misses on todo cards

--- data/src/main/java/dev/argraur/yandex/todo/data/source/MockTodoItemsDataSource.kt
+++ data/src/main/java/dev/argraur/yandex/todo/data/source/MockTodoItemsDataSource.kt
@@ -2,6 +2,8 @@ package dev.argraur.yandex.todo.data.source
 
 import dev.argraur.yandex.todo.domain.model.TodoItem
 import dev.argraur.yandex.todo.domain.model.Urgency
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.format.DateTimeFormat
 import java.util.UUID
 
 class MockTodoItemsDataSource {
@@ -11,60 +13,73 @@ class MockTodoItemsDataSource {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Urgent,
             false,
+            creationDate = LocalDate(2024, 6, 20),
+            deadline = LocalDate(2024, 6, 22)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Low,
             false,
+            creationDate = LocalDate(2024, 4, 17),
+            deadline = LocalDate(2024, 6, 20)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 4, 18)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 6, 17),
+            deadline = LocalDate(2024, 6, 22)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2023, 4, 17)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Urgent,
             false,
+            creationDate = LocalDate(2024, 5, 18)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 6, 10)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 4, 25)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 3, 17)
         ),
         TodoItem(
             UUID.randomUUID().toString(),
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             Urgency.Normal,
             false,
+            creationDate = LocalDate(2024, 2, 17)
         )
     )
 
--- ui/src/main/java/dev/argraur/yandex/todo/ui/elements/TodoCard.kt
+++ ui/src/main/java/dev/argraur/yandex/todo/ui/elements/TodoCard.kt
@@ -1,15 +1,20 @@
 package dev.argraur.yandex.todo.ui.elements
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
@@ -27,6 +32,11 @@ import dev.argraur.yandex.todo.domain.model.TodoItem
 import dev.argraur.yandex.todo.domain.model.Urgency
 import dev.argraur.yandex.todo.ui.R
 import dev.argraur.yandex.todo.ui.theme.Green
+import dev.argraur.yandex.todo.ui.theme.Red
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toKotlinLocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
 fun TodoCard(todoItem: TodoItem, onUpdateTodoItem: (TodoItem) -> Unit, onTodoItemClick: (TodoItem) -> Unit, modifier: Modifier) {
@@ -39,17 +49,40 @@ fun TodoCard(todoItem: TodoItem, onUpdateTodoItem: (TodoItem) -> Unit, onTodoIte
         Urgency.Urgent -> { text = stringResource(R.string.todo_card_urgency_urgent_format, text); uncheckedColor = Color.Red }
         else -> {}
     }
+    var deadlineMiss = todoItem.deadline != null && java.time.LocalDate.now().toKotlinLocalDate() > todoItem.deadline!!
 
     Card(modifier = modifier.fillMaxWidth(), colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)), onClick = { onTodoItemClick(todoItem) }, shape = RectangleShape) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(start = 8.dp, end = 8.dp)) {
             Checkbox(todoItem.done, onCheckedChange = { onUpdateTodoItem(todoItem.copy(done = it)) }, modifier = Modifier.weight(0.1f), colors = CheckboxDefaults.colors(checkedColor = Green, uncheckedColor = uncheckedColor))
-            Text(
-                text = text,
-                maxLines = 3,
-                modifier = Modifier.weight(0.8f).alpha(textAlpha),
-                overflow = TextOverflow.Ellipsis,
-                textDecoration = textDecoration
-            )
+            Column(modifier = Modifier.weight(0.8f)) {
+                Text(
+                    text = text,
+                    maxLines = 3,
+                    modifier = Modifier.alpha(textAlpha),
+                    overflow = TextOverflow.Ellipsis,
+                    textDecoration = textDecoration
+                )
+                if (todoItem.deadline != null) {
+                    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.alpha(0.5f), horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+                        Icon(
+                            Icons.Default.DateRange,
+                            stringResource(R.string.content_description_deadline),
+                            modifier = Modifier.size(16.dp),
+                            tint = if (deadlineMiss) Red else LocalContentColor.current
+                        )
+                        Text(
+                            color = if (deadlineMiss) Red else Color.Unspecified,
+                            text = todoItem.deadline!!.toJavaLocalDate()
+                                .format(
+                                    DateTimeFormatter.ofPattern(
+                                        stringResource(R.string.date_pattern),
+                                        Locale.getDefault()
+                                    )
+                                )
+                        )
+                    }
+                }
+            }
             Icon(Icons.Outlined.Info,
                 stringResource(R.string.content_description_info), modifier = Modifier.weight(0.1f))
         }
